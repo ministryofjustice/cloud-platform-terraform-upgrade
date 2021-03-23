fd2cc15ddee9a71b405e88429acbae466a188bca
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-03-23 20:01:48 +0000

    Add branch creation to the Clone function
    This creates a branch called "amendments", but will probably need to
    change in the future.

--- pkg/git/git.go
+++ pkg/git/git.go
@@ -2,15 +2,33 @@ package git
 
 import (
 	"github.com/go-git/go-git/v5" // with go modules enabled (GO111MODULE=on or outside GOPATH)
+	"github.com/go-git/go-git/v5/plumbing"
 )
 
 // Clone peforms a git clone into a directory called ./tmp/
-func Clone(r, d string) error {
-	url := "https://github.com/" + r
-	dir := d + "/" + r
+func Clone(repo, d string) error {
+	url := "https://github.com/" + repo
+	dir := d + "/" + repo
 
-	_, err := git.PlainClone(dir, false, &git.CloneOptions{
+	r, err := git.PlainClone(dir, false, &git.CloneOptions{
 		URL: url,
+	})
+	if err != nil {
+		return err
+	}
+
+	w, err := r.Worktree()
+	if err != nil {
+		return err
+	}
+
+	branch := "refs/heads/amendments"
+	b := plumbing.ReferenceName(branch)
+
+	err = w.Checkout(&git.CheckoutOptions{
+		Create: true,
+		Force:  false,
+		Branch: b,
 	})
 	if err != nil {
 		return err
